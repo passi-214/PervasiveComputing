7e0a586ba4cef01d1ee1120268e06b6aa5928900
Jonas <jonas.rathberger@gmx.net>
2026-04-23 14:56:40 +0200

    basic backend

---
+++ backend/build/CMakeFiles/backend_db.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/backend_db.dir/link.d"
+  "CMakeFiles/backend_db.dir/src/main.cpp.o"
+  "CMakeFiles/backend_db.dir/src/main.cpp.o.d"
+  "backend_db"
+  "backend_db.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/backend_db.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ backend/build/CMakeFiles/backend_db.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for backend_db.
+# This may be replaced when dependencies are built.
